58bddf6d1bef3facdbd78e9414fdae2393583e00
Tristan Chong <tristanchong@gmail.com>
2014-05-17 23:54:46 -0700

    use fields instead of exclude in QuoteForm

--- app/forms.py
+++ app/forms.py
@@ -6,4 +6,4 @@ from app.models import Quote, Tag
 class QuoteForm(forms.ModelForm):
     class Meta:
         model = Quote
-        exclude = ('submitter', 'num_upvotes', 'num_downvotes')
+        fields = ('text', 'tags')
