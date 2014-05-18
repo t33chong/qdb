12c68295411159a2ebb8f9680f36b1372584199e
Tristan Chong <tristanchong@gmail.com>
2014-05-17 23:01:34 -0700

    add submit/QuoteCreate route

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns(
     url('^$', views.index, name='index'),
     url('^quote/(?P<quote_id>\d+)/$', views.detail, name='detail'),
     url('^tag/(?P<tag_id>\d+)/$', views.tag, name='tag'),
+    url('^submit/$', views.QuoteCreate.as_view(), name='quote_create'),
     )
