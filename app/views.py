abe21818150cc179c062af947af422fafb64de62
Tristan Chong <tristanchong@gmail.com>
2014-05-17 21:14:51 -0700

    initial tag view

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from app.models import Quote
 
@@ -7,3 +7,9 @@ def index(request):
     quote_list = Quote.objects.all().order_by('-date')[:5]
     context = {'quote_list': quote_list}
     return render(request, 'app/index.html', context)
+
+
+def tag(request, tag_id):
+    t = get_object_or_404(Tag, pk=tag_id)
+    context = {'tag': t}
+    return render(request, 'app/tag.html', context)
