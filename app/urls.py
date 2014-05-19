314a5eaa93600dfdc03cb6b9a21e3979ad6743e3
Tristan Chong <tristanchong@gmail.com>
2014-05-18 20:34:39 -0700

    write signup route

--- app/urls.py
+++ app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns(
     url('^$', views.index, name='index'),
     url('^quote/(?P<quote_id>\d+)/$', views.detail, name='detail'),
     url('^tag/(?P<tag_id>\d+)/$', views.tag, name='tag'),
+    url('^signup/$', views.signup, name='signup'),
     url('^submit/$', views.QuoteCreate.as_view(), name='quote_create'),
     )
