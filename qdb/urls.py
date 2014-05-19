e591cf980756a3068dce1a388878cbf8f6e2f6ac
Tristan Chong <tristanchong@gmail.com>
2014-05-18 21:00:04 -0700

    add hunger to urlpatterns

--- qdb/urls.py
+++ qdb/urls.py
@@ -6,5 +6,6 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'hunger', include('hunger.urls')),
     url(r'', include('app.urls', namespace='app')),
     )
