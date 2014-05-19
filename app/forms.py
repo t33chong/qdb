b44f0ca4b1da9bef99e3bc8e1bda1ab29887bcdd
Tristan Chong <tristanchong@gmail.com>
2014-05-18 20:07:16 -0700

    initial UserForm

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,18 @@
 from django import forms
+from django.contrib.auth.models import User
 
 from app.models import Quote, Tag
 
 
+# TODO: Add ability to create new tags in quote form
 class QuoteForm(forms.ModelForm):
     class Meta:
         model = Quote
         fields = ('text', 'tags')
 
 
-# TODO: Add ability to create new tags in quote form
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
