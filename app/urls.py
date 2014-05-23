a4e317eba745a9c0fbbf5ea6489cea5c4de58161
Tristan Chong <tristanchong@gmail.com>
2014-05-22 21:53:00 -0700

    write user route

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns(
     url('^$', views.index, name='index'),
     url('^quote/(?P<quote_id>\d+)/$', views.detail, name='detail'),
     url('^tag/(?P<tag_text>\w+)/$', views.tag, name='tag'),
+    url('^user/(?P<username>\w+)/$', views.user, name='user'),
     url('^signup/$', views.signup, name='signup'),
     url('^login/$', views.log_in, name='login'),
     url('^logout/$', views.log_out, name='logout'),
