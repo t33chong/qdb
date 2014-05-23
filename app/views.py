c8a07660a41e058dc010c69cd99f08de25368fe2
Tristan Chong <tristanchong@gmail.com>
2014-05-23 07:53:23 -0700

    change page_num param to page in tag view

--- app/views.py
+++ app/views.py
@@ -35,18 +35,18 @@ def detail(request, quote_id):
 
 
 @login_required
-def tag(request, tag_text, page_num=1):
-    if page_num is None:
-        page_num = 1
+def tag(request, tag_text, page=1):
+    if page is None:
+        page = 1
     tag = Tag.objects.filter(text=tag_text).first()
-    page = None
+    page_ = None
     quotes = None
     if tag is not None:
         quotes = tag.quotes.all()
         if quotes:
             p = Paginator(quotes, PER_PAGE)
-            page = p.page(page_num)
-    context = {'tag_text': tag_text, 'page': page}
+            page_ = p.page(page)
+    context = {'tag_text': tag_text, 'page': page_}
     return render(request, 'app/tag.html', context)
 
 
