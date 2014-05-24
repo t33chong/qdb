26b5a2ce9f85c70c5299e6e867e121b1046997bc
Tristan Chong <tristanchong@gmail.com>
2014-05-23 20:39:30 -0700

    remove Upvote and Downvote models, replace with updown RatingField

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 import re
 from django.contrib.auth.models import User
 from django.db import models
+from updown.fields import RatingField
 
 
 class Tag(models.Model):
@@ -19,24 +20,7 @@ class Quote(models.Model):
     submitter = models.ForeignKey(User, related_name='quotes')
     date = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField(Tag, related_name='quotes')
-    num_upvotes = models.IntegerField(default=0)
-    num_downvotes = models.IntegerField(default=0)
+    rating = RatingField(can_change_vote=True)
 
     def __unicode__(self):
         return unicode(self.text)[:32]
-
-
-class Upvote(models.Model):
-    quote = models.ForeignKey(Quote, related_name='upvotes')
-    user = models.ForeignKey(User, related_name='upvotes')
-
-    def __unicode__(self):
-        return u'%s->%s' % (self.user.username, self.quote.text[:32])
-
-
-class Downvote(models.Model):
-    quote = models.ForeignKey(Quote, related_name='downvotes')
-    user = models.ForeignKey(User, related_name='downvotes')
-
-    def __unicode__(self):
-        return u'%s->%s' % (self.user.username, self.quote.text[:32])
