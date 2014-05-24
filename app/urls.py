e244d138f95150e3585b784af9e77ad827e5db47
Tristan Chong <tristanchong@gmail.com>
2014-05-23 22:07:33 -0700

    add vote route

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,17 @@
 from django.conf.urls import patterns, url
 
+from updown.views import AddRatingFromModel
+
 from app import views
 
 urlpatterns = patterns(
     '',
     url('^$', views.index, name='index'),
     url('^all/(?P<page_num>\d+)/$', views.index, name='all'),
+    url('^quote/(?P<quote_id>\d+)/vote/(?P<score>[\d\-]+)/$',
+        AddRatingFromModel(),
+        {'app_label': 'app', 'model': 'Quote', 'field_name': 'rating'},
+        name='vote'),
     url('^quote/(?P<quote_id>\d+)/$', views.detail, name='detail'),
     url('^tag/(?P<tag_text>\w+)/(?:(?P<page_num>\d+)/)?$',
         views.tag, name='tag'),
