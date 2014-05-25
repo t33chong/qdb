d06f674ca6973137760467aac127a0dd2e42eb28
Tristan Chong <tristanchong@gmail.com>
2014-05-25 11:18:31 -0700

    add is_safe=True to target_blank to avoid url escaping

--- app/templatetags/app_tags.py
+++ app/templatetags/app_tags.py
@@ -3,6 +3,6 @@ from django import template
 register = template.Library()
 
 
-@register.filter
+@register.filter(is_safe=True)
 def target_blank(a_tag):
     return a_tag.replace('<a ', '<a target="_blank" ')
