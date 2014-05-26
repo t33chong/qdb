41b5b90f4cedb7a8dd0d9d586df94821c3998141
Tristan Chong <tristanchong@gmail.com>
2014-05-25 20:45:04 -0700

    change upvote and downvote routes to rate

--- app/urls.py
+++ app/urls.py
@@ -14,6 +14,5 @@ urlpatterns = patterns(
     url('^login/$', views.log_in, name='login'),
     url('^logout/$', views.log_out, name='logout'),
     url('^submit/$', views.submit, name='submit'),
-    url('^upvote/(?P<quote_id>\d+)/$', views.upvote, name='upvote'),
-    url('^downvote/(?P<quote_id>\d+)/$', views.downvote, name='downvote'),
+    url('^rate/(?P<quote_id>\d+)/$', views.rate, name='rate'),
     )
