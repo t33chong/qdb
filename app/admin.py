b8289d02088cd6f7451c3569191cab2de2ca8022
Tristan Chong <tristanchong@gmail.com>
2014-05-26 14:23:48 -0700

    register Vote in admin

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
+from updown.models import Vote
 from app.models import Tag, Quote
 
 admin.site.register(Tag)
 admin.site.register(Quote)
+admin.site.register(Vote)
