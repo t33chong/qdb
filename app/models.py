2807409e48821978fa7d0b68c82dbad398c9c95e
Tristan Chong <tristanchong@gmail.com>
2014-05-25 17:33:30 -0700

    remove Upvote/Downvote models; enable secretballot voting on Quote

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from djorm_pgfulltext.models import SearchManager
 from djorm_pgfulltext.fields import VectorField
 
+import secretballot
+
 
 class Tag(models.Model):
     text = models.CharField(max_length=64, unique=True)
@@ -21,8 +23,6 @@ class Quote(models.Model):
     submitter = models.ForeignKey(User, related_name='quotes')
     date = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField(Tag, related_name='quotes')
-    num_upvotes = models.IntegerField(default=0)
-    num_downvotes = models.IntegerField(default=0)
 
     search_index = VectorField()
     objects = models.Manager()
@@ -34,17 +34,4 @@ class Quote(models.Model):
         return unicode(self.text)[:32]
 
 
-class Upvote(models.Model):
-    quote = models.ForeignKey(Quote, related_name='upvotes')
-    user = models.ForeignKey(User, related_name='upvotes')
-
-    def __unicode__(self):
-        return u'%s->%s' % (self.user.username, self.quote.text[:32])
-
-
-class Downvote(models.Model):
-    quote = models.ForeignKey(Quote, related_name='downvotes')
-    user = models.ForeignKey(User, related_name='downvotes')
-
-    def __unicode__(self):
-        return u'%s->%s' % (self.user.username, self.quote.text[:32])
+secretballot.enable_voting_on(Quote)
