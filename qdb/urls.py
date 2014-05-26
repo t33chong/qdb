8554d3481f392cfb0086918e95321a86d13e1f1e
Tristan Chong <tristanchong@gmail.com>
2014-05-25 17:38:19 -0700

    forgot closing parenthesis

--- qdb/urls.py
+++ qdb/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^password_required/$', 'password_required.views.login'),
-    url(r'^likes/$', include('likes.urls'),
+    url(r'^likes/$', include('likes.urls')),
     url(r'', include('app.urls', namespace='app')),
     )
